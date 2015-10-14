cd42bc2d315f2381b648d51846e20f550df27f2d
Michael Wang <michaelwang0712@gmail.com>
2015-10-14 12:33:37 -0500

    Flatten the tree into a list of lists by level

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,30 @@
-// $(document).click(function(e) {
-//   /* Inform the backgrund page that
-//  * this tab should have a page-action */
-  chrome.runtime.sendMessage({
+$(document).click(function(e) {
+    // BFS the DOM, building a list of lists of the tree by level
+    var treeByLevel = [];
+    var q = [];
+    q.push([0, e.target]);
+
+    while (q.length !== 0) {
+        var node = q.pop();
+        var level = node[0];
+        var element = node[1];
+
+        if (!element) continue;     // leaf node
+        treeByLevel[level] = treeByLevel[level] || [];
+        treeByLevel[level].push(element);
+
+        for (var i = 0; i < element.children.length; i++) {
+            q.push([level+1, element.children[i]]);
+        }
+    }
+
+    chrome.runtime.sendMessage({
       from:    'content',
       subject: 'showPageAction'
-  });
+    });
 
-  /* Listen for message from the popup */
-  chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+    /* Listen for message from the popup */
+    chrome.runtime.onMessage.addListener(function(msg, sender, response) {
       /* First, validate the message's structure */
       if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
           /* Collect the necessary data
@@ -22,5 +39,5 @@
            * through the specified callback */
           response(domInfo);
       }
-  });
-// });
+    });
+});
